c41953aa09369771807f2689b4cfa20ec67b5692
Ramjdeemp <avatarxsonofgod@gmail.com>
2026-07-30 13:36:17 +0530

    Update API_URL to use production URL only

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,2 @@
 export const API_URL =
-    location.hostname === "localhost"
-        ? "http://localhost:3000"
-        : "https://secure-vault-production-3662.up.railway.app";
+    "https://secure-vault-production-3662.up.railway.app";
